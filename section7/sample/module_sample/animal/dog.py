5e3e3a12130ceb988d34e6dddb4981e92033c566
crashRT <crashrt.farbauti@gmail.com>
2023-05-31 01:30:59 +0900

    パッケージ

---
+++ section7/sample/module_sample/animal/dog.py
@@ -0,0 +1,6 @@
+class Doog:
+    def __init__(self, name):
+        self.name = name
+
+    def woof(self):
+        print("わんわん！")
---
+++ section7/sample/module_sample/app.py
@@ -0,0 +1,7 @@
+from animal import dog, cat
+
+tama = cat.Cat("タマ")
+tama.meow()
+
+poti = dog.Dog("ポチ")
+poti.woof()
